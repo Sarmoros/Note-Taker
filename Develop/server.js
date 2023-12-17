1f93a4d61e2679515df27516f0d4e3b557066e69
Sarahy M <sarahym0703@gmail.com>
2023-12-17 17:50:21 -0500

    added a route.js file to keep all routes in tha seperate file

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const fs = require("fs");
+const routes = require("./routes");
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+
+// Starts cthe server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
